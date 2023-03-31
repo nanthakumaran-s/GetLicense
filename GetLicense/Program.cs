0a389ecce9d38887f1764a9f761eb8e885abedfd
nanthakumaran-s <nanthankumaransenthil@gmail.com>
2023-03-31 16:14:46 +0530

    Initial commit

---
+++ GetLicense/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(GetLicense("Eric", 2, "Adam Caroline Rebecca Frank"));
+        Console.WriteLine(GetLicense("Zebediah", 1, "Bob Jim Becky Pat"));
+        Console.WriteLine(GetLicense("Aaron", 3, "Jane Max Olivia Sam"));
+    }
+
+    public static int GetLicense(string myself, int noOfAgents, string others)
+    {
+        List<string> names = others.Split(' ').Append(myself).OrderBy(name => name).ToList();
+        Print(names);
+        var myIndex = names.IndexOf(myself);
+        return ((myIndex / noOfAgents) + 1) * 20;
+    }
+
+    public static void Print(List<string> name)
+    {
+        foreach (var n in name)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ GetLicenseTests/ProgramTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            Assert.IsTrue(Program.GetLicense("Eric", 2, "Adam Caroline Rebecca Frank") == 40);
+        }
+
+        [TestMethod()]
+        public void Test2()
+        {
+            Assert.IsTrue(Program.GetLicense("Zebediah", 1, "Bob Jim Becky Pat") == 100);
+        }
+
+        [TestMethod()]
+        public void Test3()
+        {
+            Assert.IsTrue(Program.GetLicense("Aaron", 3, "Jane Max Olivia Sam") == 20);
+        }
+    }
+}
